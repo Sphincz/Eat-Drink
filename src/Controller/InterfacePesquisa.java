667705877213f46099fec18d5718cc7438f786ff
Sphincz <raimundo.asl@gmail.com>
2014-04-22 16:46:43 +0100

    GUI: Updated v2

--- src/Controller/InterfacePesquisa.java
+++ src/Controller/InterfacePesquisa.java
@@ -13,6 +13,7 @@ import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import javax.swing.JButton;
 
@@ -39,17 +40,17 @@ public class InterfacePesquisa extends JFrame {
 		getContentPane().add(lblUtilizador);
 		
 		textField = new JTextField();
-		textField.setBounds(80, 23, 147, 20);
+		textField.setBounds(80, 23, 200, 20);
 		getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblEstabelecimento = new JLabel("Estabelecimento:");
-		lblEstabelecimento.setBounds(261, 26, 82, 14);
+		lblEstabelecimento.setBounds(334, 29, 82, 14);
 		getContentPane().add(lblEstabelecimento);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(353, 23, 170, 20);
+		textField_1.setBounds(426, 26, 185, 20);
 		getContentPane().add(textField_1);
 		
 		JLabel lblPrato = new JLabel("Prato:");
@@ -58,23 +59,23 @@ public class InterfacePesquisa extends JFrame {
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
-		textField_2.setBounds(80, 58, 147, 20);
+		textField_2.setBounds(80, 58, 200, 20);
 		getContentPane().add(textField_2);
 		
 		JLabel lblAvaliao = new JLabel("Avalia\u00E7\u00E3o >=");
-		lblAvaliao.setBounds(271, 61, 68, 14);
+		lblAvaliao.setBounds(348, 61, 68, 14);
 		getContentPane().add(lblAvaliao);
 		
 		JSpinner spinner = new JSpinner();
-		spinner.setBounds(353, 58, 78, 20);
+		spinner.setBounds(426, 58, 78, 20);
 		getContentPane().add(spinner);
 		
 		JLabel lblFotografia = new JLabel("Fotografia:");
-		lblFotografia.setBounds(449, 61, 58, 14);
+		lblFotografia.setBounds(537, 64, 58, 14);
 		getContentPane().add(lblFotografia);
 		
 		JCheckBox checkBox = new JCheckBox("");
-		checkBox.setBounds(502, 56, 21, 23);
+		checkBox.setBounds(590, 59, 21, 23);
 		getContentPane().add(checkBox);
 		
 		JLabel lblComentrio = new JLabel("Coment\u00E1rio:");
@@ -83,15 +84,15 @@ public class InterfacePesquisa extends JFrame {
 		
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
-		textField_3.setBounds(80, 92, 147, 20);
+		textField_3.setBounds(80, 92, 200, 20);
 		getContentPane().add(textField_3);
 		
 		JLabel lblPesquisar = new JLabel("Pesquisar:");
-		lblPesquisar.setBounds(291, 95, 52, 14);
+		lblPesquisar.setBounds(364, 95, 52, 14);
 		getContentPane().add(lblPesquisar);
 		
 		JButton btnEstabelecimento = new JButton("Estabelecimento");
-		btnEstabelecimento.setBounds(353, 91, 110, 23);
+		btnEstabelecimento.setBounds(426, 91, 110, 23);
 		getContentPane().add(btnEstabelecimento);
 		
 		JButton btnPrato = new JButton("Prato");
@@ -99,7 +100,7 @@ public class InterfacePesquisa extends JFrame {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		btnPrato.setBounds(463, 91, 60, 23);
+		btnPrato.setBounds(547, 91, 64, 23);
 		getContentPane().add(btnPrato);
 		
 		JButton btnNewButton = new JButton("Ver fotografia");
@@ -111,17 +112,17 @@ public class InterfacePesquisa extends JFrame {
 		getContentPane().add(btnVer);
 		
 		JButton btnAdicionarComentrio = new JButton("Adicionar coment\u00E1rio");
-		btnAdicionarComentrio.setBounds(227, 444, 170, 23);
+		btnAdicionarComentrio.setBounds(220, 444, 170, 23);
 		getContentPane().add(btnAdicionarComentrio);
 		
 		JButton btnAdicionarFotografia = new JButton("Adicionar fotografia");
-		btnAdicionarFotografia.setBounds(227, 478, 170, 23);
+		btnAdicionarFotografia.setBounds(220, 478, 170, 23);
 		getContentPane().add(btnAdicionarFotografia);
 		
 		JButton btnSair = new JButton("Sair");
-		btnSair.setBounds(463, 478, 60, 23);
+		btnSair.setBounds(551, 478, 60, 23);
 		getContentPane().add(btnSair);
-
+		
 		String[] columnNames = {"Utilizador",
                 "Estabelecimento",
                 "Prato",
@@ -129,9 +130,10 @@ public class InterfacePesquisa extends JFrame {
                 "Comentário",
                 "Fotografia"};
 
+		
 		Object[][] data = {
 				{"João Camexias", "Toni",
-					"Snowboarding", new Integer(5), "É uma merda!",  new Boolean(false)},
+					"Snowboarding", new Integer(5), "É uma merda!",  new Boolean(true)},
 					{"Adolfo Dias", "Toni",
 						"Rowing", new Integer(3), "É uma merda!", new Boolean(true)},
 						{"Oscar Alho", "Toni",
@@ -141,16 +143,30 @@ public class InterfacePesquisa extends JFrame {
 								{"E eu", "Toni",
 									"Pool", new Integer(10), "É uma merda!", new Boolean(false)}
 		};
+
+		DefaultTableModel tableModel = new DefaultTableModel(data, columnNames) {
+			private static final long serialVersionUID = 1L;
+			@Override
+		    public boolean isCellEditable(int row, int column) {
+		        //all cells false
+		        return false;
+		    }
+			@Override
+			public Class<?> getColumnClass(int columnIndex) {
+			    if (columnIndex == 5)
+			        return Boolean.class;
+			    return super.getColumnClass(columnIndex);
+			}
+		};
+		table = new JTable(tableModel);
+		table.setFillsViewportHeight(true);
+		table.getTableHeader().setReorderingAllowed(false);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(21, 133, 490, 289);
-		getContentPane().add(scrollPane);
-		
-
-		table = new JTable(data, columnNames);
+		scrollPane.setBounds(21, 133, 590, 289);
 		scrollPane.setViewportView(table);
-		table.setFillsViewportHeight(true);
-		table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+		getContentPane().add(scrollPane);
+
 	}
 	
 	private static final long serialVersionUID = 1L;
@@ -173,7 +189,7 @@ public class InterfacePesquisa extends JFrame {
 				try {
 					InterfacePesquisa frame = new InterfacePesquisa();
 					frame.setVisible(true);
-					frame.setSize(550, 550);
+					frame.setSize(650, 550);
 					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				} catch (Exception e) {
 					e.printStackTrace();
